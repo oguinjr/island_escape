366feaa19a645824d795afd23774098ba5476465
oguinjr <frankoguinjr@gmail.com>
2022-03-14 01:04:49 -0700

    Add files via upload

--- island.py
+++ island.py
@@ -1,62 +1,62 @@
 def all_dead():
-	print("You left all the animals to eat each other up.")
-	print("You die of starvation.")
-	print("........")
-	print("Hit enter to quit.")
-	input()
-	exit(0)
+	if 'fox' in main_land:
+		if 'chicken' in main_land:
+			if 'grain' in main_land:
+				print("You left all the animals to eat each other up.")
+				print("You die of starvation.")
+				print("........")
+				print("Hit enter to quit.")
+				input()
+				exit(0)
 
 def dead_chicken_main():
-	if ('fox') and ('chicken') in [main_land]:
-		print("You left the fox to eat the chicken.")
-		print("I hope you enjoy eating your grain!")
-		print("Hit enter to quit.")
-		input()
-		exit(0)
-
+	if 'fox' in main_land:
+		if 'chicken' in main_land:
+				print("You left the fox to eat the chicken.")
+				print("I hope you enjoy eating your grain!")
+				print("Hit enter to quit.")
+				input()
+				exit(0)
 
 def dead_chicken_island():
-	if ('fox') and ('chicken') in [island]:
-		print("You left the fox to eat the chicken.")
-		print("I hope you enjoy eating your grain!")
-		print("Hit enter to quit.")
-		input()
-		exit(0)
-	# why did I have to add this line below?
-	elif ('fox') and ('grain') in [island]:
-		print("chicken on island is fine")
-		input()
+	if 'fox' in island:
+		if 'chicken' in island:
+			print("You left the fox to eat the chicken.")
+			print("I hope you enjoy eating your grain!")
+			print("Hit enter to quit.")
+			input()
+			exit(0)
 
 def no_grain_main():
-	if ('chicken') and ('grain') in [main_land]:
-		print("You left the chicken to eat the grain.")
-		print("I hope you enjoy growing no crops!")
-		print("Hit enter to quit.")
-		input()
-		exit(0)
-
-	elif ('fox') and ('grain') in [main_land]:
-		print("It's all good")
+	if 'chicken' in main_land:
+		if 'grain' in main_land:
+			print("You left the chicken to eat the grain.")
+			print("I hope you enjoy growing no crops!")
+			print("Hit enter to quit.")
+			input()
+			exit(0)
 
 def no_grain_island():
-	if ('chicken') and ('grain') in [island]:
-		print("You left the chicken to eat the grain.")
-		print("I hope you enjoy growing no crops!")
-		print("Hit enter to quit.")
-		input()
-		exit(0)
-
-	elif ('fox') and ('grain') in [island]:
-		print("island looks good.")
-		input()
+	if 'chicken' in island:
+		if 'grain' in island:
+			print("You left the chicken to eat the grain.")
+			print("I hope you enjoy growing no crops!")
+			print("Hit enter to quit.")
+			input()
+			exit(0)
 
 def set_sail():
+	dead_chicken_main()
+	dead_chicken_island()
+	no_grain_main()
+	no_grain_island()
 	print("Hit enter to set sail.")
 	input()
 
 island = []
 main_land = ['fox','grain','chicken']
 boat = []
+line_break = print("------------------------------------------------------------------")
 def island_escape():
 	print("""You need to escape from the mainland to a nearby island with your\n
 	--------------------------------------------------------------------------\n
@@ -76,18 +76,24 @@ def island_escape():
 			print("Press Enter to continue.")
 			input()
 			print("Do you want to take an animal or item with you across the river? Please type y or n")
+			print("-----------------------------------------------------------")
 			choice = input('>')
 			if choice == 'y':
+				print("-------------------------------------------------------")
 				for x in main_land:
 					print(x)
+				print("-----------------------------------------------------------")
 				print("Of the animals or items listed type the name which you would like to set sail with.")
 				choice = input('>')
 				main_land.remove(choice)
 				boat.append(choice)
+				print("-----------------------------------------------------------")
 				print(f"The boat has {boat} and the mainland has {main_land}.")
-				no_grain_main()
-				dead_chicken_main()
+				print("-----------------------------------------------------------")
 				set_sail()
+				print("-----------------------------------------------------------")
+
+
 
 
 			elif choice == 'n':
@@ -95,8 +101,6 @@ def island_escape():
 				if 'fox' and 'chicken' and 'grain' in main_land:
 					all_dead()
 				else:
-					no_grain_main()
-					dead_chicken_main()
 					set_sail()
 			else:
 				print("Maybe you typed something wrong?")
@@ -104,6 +108,7 @@ def island_escape():
 
 		else:
 			print(f"Do you want to:")
+			print("-----------------------------------------------------------")
 			if len(main_land) == 2:
 				swap_first = main_land[0]
 				swap_second = main_land[1]
@@ -111,25 +116,30 @@ def island_escape():
 				print(f"a. Drop off the {boat} and leave alone?")
 				print(f"b. swap the {boat_first} for the {swap_first}")
 				print(f"c. swap the {boat_first} for the {swap_second}")
+				print("-----------------------------------------------------------")
 				choice = input('>')
+				print("-----------------------------------------------------------")
 			elif len(main_land) == 1:
 				swap_first = main_land[0]
 				boat_first = boat[0]
 				print(f"a. Drop off the {boat} and leave alone?")
 				print(f"b. swap the {boat_first} for the {swap_first}")
+				print("-----------------------------------------------------------")
 				choice = input('>')
+				print("-----------------------------------------------------------")
 			else:
 				print(f"a. Drop off the {boat} and leave alone?")
+				print("-----------------------------------------------------------")
 				choice = input('>')
+				print("-----------------------------------------------------------")
 
 			if choice == 'a':
 				boat_first = boat[0]
 				main_land.append(boat_first)
 				boat.remove(boat_first)
 				print(f"The boat now has {boat} and the mainland has {main_land}.")
-				dead_chicken_main()
-				no_grain_main()
 				set_sail()
+				print("-----------------------------------------------------------")
 
 			elif choice == 'b':
 				main_first = main_land[0]
@@ -139,8 +149,7 @@ def island_escape():
 				main_land.append(boat_first)
 				boat.remove(boat_first)
 				print(f"The boat now has {boat} and the mainland has {main_land}.")
-				dead_chicken_main()
-				no_grain_main()
+				print("-----------------------------------------------------------")
 				set_sail()
 			elif choice == 'c':
 				main_second = main_land[1]
@@ -149,8 +158,7 @@ def island_escape():
 				main_land.remove(main_second)
 				boat.remove(boat_first)
 				print(f"The boat now has {boat} and the mainland has {main_land}.")
-				dead_chicken_main()
-				no_grain_main()
+				print("-----------------------------------------------------------")
 				set_sail()
 
 		if not boat:
@@ -159,32 +167,38 @@ def island_escape():
 			input()
 			if not island:
 				print("What are you doing? You shouldnt have made it here")
+				print("-----------------------------------------------------------")
 			else:
 				print("Do you want to take an animal or item with you across the river? Please type y or n")
+				print("-----------------------------------------------------------")
 			choice = input('>')
 			if choice == 'y':
+				print("-----------------------------------------------------------")
 				for x in island:
 					print(x)
+					print("-----------------------------------------------------------")
 					print("Of the animals or items listed type the name which you would like to set sail with.")
 				choice = input('>')
 				island.remove(choice)
 				boat.append(choice)
+				print("-----------------------------------------------------------")
 				print(f"The boat has {boat} and the island has {island}.")
-				dead_chicken_island()
-				no_grain_island()
+				print("-----------------------------------------------------------")
 				set_sail()
 
 			elif choice == 'n':
+				print("-----------------------------------------------------------")
 				print(f"Just messing around I see.")
-				dead_chicken_island()
-				no_grain_island()
+				print("-----------------------------------------------------------")
 				set_sail()
 
 		else:
 			print("You are at the island.")
 			print("Press Enter to continue.")
 			input()
+			print("-----------------------------------------------------------")
 			print(f"Do you want to:")
+			print("-----------------------------------------------------------")
 			if len(island) == 2:
 				swap_first = island[0]
 				swap_second = island[1]
@@ -193,30 +207,39 @@ def island_escape():
 				print(f"b. swap the {boat_first} for the {swap_first}")
 				print(f"c. swap the {boat_first} for the {swap_second}")
 				print(f"z. Keep the {boat} and head back to mainland")
+				print("-----------------------------------------------------------")
 				choice = input('>')
+				print("-----------------------------------------------------------")
 			elif len(island) == 1:
 				swap_first = island[0]
 				boat_first = boat[0]
 				print(f"a. Drop off the {boat} and leave alone?")
 				print(f"b. swap the {boat_first} for the {swap_first}")
 				print(f"z. Keep the {boat_first} and head back to mainland")
+				print("-----------------------------------------------------------")
 				choice = input('>')
+				print("-----------------------------------------------------------")
 			else:
 				print(f"a. Drop off the {boat} and leave alone?")
 				print(f"z. Keep the {boat} and head back to mainland")
+				print("-----------------------------------------------------------")
 				choice = input('>')
+				print("-----------------------------------------------------------")
 
 			if choice == 'a':
 				boat_first = boat[0]
 				island.append(boat_first)
 				boat.remove(boat_first)
 				print(f"The boat now has {boat} and the island has {island}.")
-				if ('fox','chicken','grain') in [island]:
-					print('you won!')
-					break
+				if 'grain' in island:
+					if 'fox' in island:
+						if 'chicken' in island:
+							print("-----------------------------------------------------------")
+							print('you won!')
+							print("-----------------------------------------------------------")
+
+							exit(0)
 				else:
-					dead_chicken_island()
-					no_grain_island()
 					set_sail()
 			elif choice == 'b':
 				island_first = island[0]
@@ -225,9 +248,10 @@ def island_escape():
 				island.remove(island_first)
 				island.append(boat_first)
 				boat.remove(boat_first)
+				print("-----------------------------------------------------------")
 				print(f"The boat now has {boat} and the island has {island}.")
-				dead_chicken_island()
-				no_grain_island()
+				print("-----------------------------------------------------------")
+
 				set_sail()
 			elif choice == 'c':
 				island_second = island[1]
@@ -235,9 +259,10 @@ def island_escape():
 				boat.append(island_second)
 				island.remove(island_second)
 				boat.remove(boat_first)
+				print("-----------------------------------------------------------")
 				print(f"The boat now has {boat} and the mainland has {island}.")
-				dead_chicken_island()
-				no_grain_island()
+				print("-----------------------------------------------------------")
+
 				set_sail()
 			else:
 				print("Alright then, back to the mainland")
